ee97e750dc51d12b3699baead5eb40f5dfe0d09a
gubnich <gubnich@yandex.ru>
2018-09-29 16:01:14 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  if(number < 1 || number > 1E8) return 0;
+  number = parseInt(number / 5);
+  return number += getZerosCount(number);
 }
